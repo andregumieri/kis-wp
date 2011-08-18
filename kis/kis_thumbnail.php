5d86b314465242f4ed4aa9e8d1f0b962f9820b86
André Gumieri <andregumieri@elodigital.cc>
2011-08-18 12:49:50 -0300

    ADD: kis_thumbnail. Funções de thumbnail.
    Signed-off-by: André Gumieri <andregumieri@elodigital.cc>

---
+++ kis/kis_thumbnail.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * KIS: Wordpress - Tumbnail Functions
+ *
+ * Related thumbnails functions
+ *
+ * @author André Gumieri
+ * @version 1.0
+ *
+ * @package KIS
+ * @subpackage Thumbnail
+ */
+
+
+/**
+ * Retrieve the URL from a post thumbnail.
+ *
+ * @author André Gumieri
+ * @since 1.0
+ *
+ * @param string $post_id The ID of the post.
+ * @param mixed $size The string of a thumbnail size or a array with width and height.
+ * @return mixed false when has no thumbnail or string when has.
+ */
+function kis_get_thumbnail_url($post_id, $size='large') {
+	$image_id = get_post_thumbnail_id($post_id);  
+	if(!empty($image_id)) {
+		$image_url = wp_get_attachment_image_src($image_id, $size);  
+		$image_url = $image_url[0];
+		return $image_url;
+	} else {
+		return false;
+	}
+}
+?>
