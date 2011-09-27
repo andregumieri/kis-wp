ca5d476f456a787e18c26608013d0cb5ae790848
André Gumieri <andregumieri@elodigital.cc>
2011-09-27 16:49:21 -0300

    Adicionada library kis.strings.js que contem funções para manipulação de strings.
    Signed-off-by: André Gumieri <andregumieri@elodigital.cc>

---
+++ assets/js/libs/kis.strings.js
@@ -0,0 +1,57 @@
+/**
+ * Classe com funções para strings
+ *
+ * @version 1.0
+ */
+
+var KisStrings = {
+	/**
+	 * Função para truncar uma string sem cortar a última palavra
+	 *
+	 * @param string (string): String com o texto a ser truncado
+	 * @param token (string): Tamanho do texto a ser truncado
+	 * @param strtruncate (string): String a ser colocada após truncar a string
+	 *
+	 * @return string: Nova string substituida
+	 */
+	truncate : function(string, size, strtruncate) {
+		if(string.length<=size) {
+			return string;
+		}
+		
+		var validaCaractereFinal = function(char) {
+			if(char==" " || char=="." || char==";") {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		var ultimoCaractere = string.substr(size-1,1);
+		if(validaCaractereFinal(ultimoCaractere)==true) {
+			return string;
+		}
+		
+		var proximoCaractere = string.substr(size,1); // Caractere que vem logo após o truncate da string
+		var encontrado = validaCaractereFinal(proximoCaractere);
+
+		
+		while(encontrado==false) {
+			size++;
+			proximoCaractere = string.substr(size,1);
+			encontrado = validaCaractereFinal(proximoCaractere);
+
+			if(size==string.length) {
+				encontrado = true;	
+				strtruncate = "";
+			}
+		}
+		
+		ultimoCaractere = string.substr(size-1,1);
+		if(ultimoCaractere==".") {
+			strtruncate="";
+		}
+		
+		return string.substr(0,size)+strtruncate;
+	}
+};
