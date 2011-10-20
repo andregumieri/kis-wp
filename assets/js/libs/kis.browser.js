107ea0ec57a2dd145175973c5ed0a174dc6eb69f
André Gumieri <andregumieri@gmail.com>
2011-10-19 22:56:58 -0200

    JS KIS Browser 2.0 - Modificado a classe para ser um sub package de Kis, sendo chamada ao invés de KisBrowser por Kis.Browser.
    Signed-off-by: André Gumieri <andregumieri@gmail.com>

--- assets/js/libs/kis.browser.js
+++ assets/js/libs/kis.browser.js
@@ -1,85 +1,94 @@
 /**
  * Classe com funções de navegação
  *
- * @version 1.1
+ * @version 2.0
+ *
+ * @package Kis
+ * @subpackage Browser
  */
- 
-var KisBrowser = {
 
-	/**
-	 * Função para saber a versão do IE
-	 * @author André Gumieri
-	 * @sinse 1.0
-	 *
-	 * @return string: Versão do IE ou false caso não seja IE
-	 */
-	IEVersion: function() {
-		var version = 999; // we assume a sane browser
-		if (navigator.appVersion.indexOf("MSIE") != -1) {
-			// bah, IE again, lets downgrade version number
-			version = parseFloat(navigator.appVersion.split("MSIE")[1]);
-			return version;
-		} else {
-			return false;
-		}
-	},
+if(!window['Kis']) { var Kis={} }
+(function(namespace) {
+	namespace.Browser = {
+		/**
+		 * ieVersion()
+		 *
+		 * Função para saber a versão do IE
+		 * @author André Gumieri
+		 * @sinse 1.0
+		 *
+		 * @return string: Versão do IE ou false caso não seja IE
+		 */
+		ieVersion: function() {
+			var version = 999; // we assume a sane browser
+			if (navigator.appVersion.indexOf("MSIE") != -1) {
+				// bah, IE again, lets downgrade version number
+				version = parseFloat(navigator.appVersion.split("MSIE")[1]);
+				return version;
+			} else {
+				return false;
+			}
+		},
+		
 	
-
-	/**
-	 * Função para saber a largura útil do navegador. Compatível com IE6+
-	 * Adaptado do site do Andy Langton
-	 * 
-	 * @author André Gumieri
-	 * @sinse 1.1
-	 * @link http://andylangton.co.uk/articles/javascript/get-viewport-size-javascript/
-	 *
-	 * @return int: Largura útil
-	 */	
-	InnerWidth: function() {
-		var viewportwidth;
-		if (typeof window.innerWidth != 'undefined') {
-			// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-			viewportwidth = window.innerWidth;
-		} else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
-			// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-			viewportwidth = document.documentElement.clientWidth;
-		} else {
-			// older versions of IE
-			viewportwidth = document.getElementsByTagName('body')[0].clientWidth;
+		/**
+		 * innerWidth()
+		 *
+		 * Função para saber a largura útil do navegador. Compatível com IE6+
+		 * Adaptado do site do Andy Langton
+		 * 
+		 * @author André Gumieri
+		 * @sinse 1.1
+		 * @link http://andylangton.co.uk/articles/javascript/get-viewport-size-javascript/
+		 *
+		 * @return int: Largura útil
+		 */	
+		innerWidth: function() {
+			var viewportwidth;
+			if (typeof window.innerWidth != 'undefined') {
+				// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+				viewportwidth = window.innerWidth;
+			} else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
+				// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+				viewportwidth = document.documentElement.clientWidth;
+			} else {
+				// older versions of IE
+				viewportwidth = document.getElementsByTagName('body')[0].clientWidth;
+			}
+			
+			return viewportwidth;
+		},
+	
+	
+	
+		/**
+		 * innerHeight()
+		 *
+		 * Função para saber a altura útil do navegador. Compatível com IE6+
+		 * Adaptado do site do Andy Langton
+		 * 
+		 * @author André Gumieri
+		 * @sinse 1.1
+	 	 * @link http://andylangton.co.uk/articles/javascript/get-viewport-size-javascript/
+		 *
+		 *
+		 * @return int: Altura útil
+		 */		
+		innerHeight: function() {
+			var viewportheight;
+	
+			if (typeof window.innerWidth != 'undefined') {
+				// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+				viewportheight = window.innerHeight;
+			} else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
+				// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+				viewportheight = document.documentElement.clientHeight;
+			} else {
+				// older versions of IE
+				viewportheight = document.getElementsByTagName('body')[0].clientHeight;
+			}
+			
+			return viewportheight;
 		}
-		
-		return viewportwidth;
-	},
-
-
-
-	/**
-	 * Função para saber a altura útil do navegador. Compatível com IE6+
-	 * Adaptado do site do Andy Langton
-	 * 
-	 * @author André Gumieri
-	 * @sinse 1.1
- 	 * @link http://andylangton.co.uk/articles/javascript/get-viewport-size-javascript/
-	 *
-	 *
-	 * @return int: Altura útil
-	 */		
-	InnerHeight: function() {
-		var viewportheight;
-
-		if (typeof window.innerWidth != 'undefined') {
-			// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-			viewportheight = window.innerHeight;
-		} else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
-			// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-			viewportheight = document.documentElement.clientHeight;
-		} else {
-			// older versions of IE
-			viewportheight = document.getElementsByTagName('body')[0].clientHeight;
-		}
-		
-		return viewportheight;
 	}
-	
-	
-};
+})(Kis);
