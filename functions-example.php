4f07cdae836f6029d5d1ee9235fb9f4a9767e719
André Gumieri <andregumieri@gmail.com>
2011-11-21 18:15:30 -0200

    Alguns exemplos de como utilizar funções do KIS
    Signed-off-by: André Gumieri <andregumieri@gmail.com>

---
+++ functions-example.php
@@ -0,0 +1,35 @@
+<?php
+	////////////////////////////////
+	// KIS CPT
+	////////////////////////////////
+	
+	// Registra novo custom post type (CPT)
+	// Param 1 - Slug do CPT
+	// Param 2 - Título (plural) do CPT
+	// Param 3 - Título (singular) do CPT
+	// Param 4 - Array dos campos a serem exibidos
+	kis_cpt_register("projetos", "Projetos", "Projeto", array("revision","title","thumbnail"));
+	
+	// Registra a taxonomia (categoria ou tags)
+	// Param 1 - Slug do CPT registrado acima
+	// Param 2 - Slug da taxonomia a ser registrada
+	// Param 3 - Título (singular) da taxonomia dentro do CPT
+	// Param 4 - Título (plural) da taxonomia dentro do CPT
+	kis_cpt_add_taxonomy("projetos", "projetos-categorias", "Categoria", "Categorias");
+	
+	// Adiciona uma metabox de campos personalizados no CPT
+	// Param 1 - Slug do CPT registrado acima
+	// Param 2 - Slug da metabox
+	// Param 3 - Título da metabox
+	kis_cpt_add_meta_box("projetos", "informacoes", "Informações");
+	
+	// Adiciona um campo na metabox registrada acima
+	// Param 1 - Slug do CPT que tem a metabox
+	// Param 2 - Slug da metabox dentro do CPT informado
+	// Param 3 - Slug (name) do campo adicionado
+	// Param 4 - Título do campo adicionado
+	// Param 5 - Tipo do campo adicionado
+	// Param 6 (opcional) - Valor padrão do campo, ou valor selecionado em checkbox, radio e option.
+	// Param 7 (opcional) - Array de opções para determinados tipos de campos
+	kis_cpt_add_field("projetos", "informacoes", "descricao", "Descrição", "textarea");
+?>
--- functions.php
+++ functions.php
@@ -1,6 +1,13 @@
 <?php
 require("kis/kis.php"); // Funções padrões do KIS (Required)
-require("kis/kis_thumbnail.php"); // Módulo de thumbnails do post
+require("kis/kis_cpt.php"); // Módulo de custom post type
+require("kis/kis_manager.php"); // Módulo de painel de settings
+require("kis/kis_minify.php"); // Módulo para fazer minify de scripts e css
 require("kis/kis_string.php"); // Módulo com funções de strings
+require("kis/kis_thumbnail.php"); // Módulo de thumbnails do post
 
+
+// Alguns Exemplos de uso das funções do KIS.
+// Apague esta linha e este arquivo quando for programar
+require("functions-example.php");
 ?>
