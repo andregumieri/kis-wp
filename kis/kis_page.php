b52dfcdf1e68f01d8f16ed631e0a7048fde3d351
André Gumieri <andregumieri@elodigital.cc>
2012-01-19 17:36:43 -0200

    Atualização no KIS Page
    Signed-off-by: André Gumieri <andregumieri@elodigital.cc>

--- kis/kis_page.php
+++ kis/kis_page.php
@@ -5,7 +5,7 @@
  * Page related functions
  *
  * @author André Gumieri
- * @version 1.0
+ * @version 1.0.1
  *
  * @package KIS
  * @subpackage Page
@@ -16,7 +16,7 @@
  * Monta o breadcrumb através das páginas
  *
  * @author André Gumieri
- * @since 1.0
+ * @since 1.0.1
  *
  * @param mixed $home FALSE caso não seja para mostrar a página inicial ou a string do texto da página inicial. Default: Página inicial.
  * @param mixed $container FALSE caso não seja mostrar o container, ou a string com o nome da tag. Default: FALSE.
@@ -35,7 +35,7 @@ function kis_page_breadcrumb($home="Página inicial", $container=false) {
 		$pg = get_page($a);
 		$bc .= "<a href=\"" . get_page_link($a) . "\"><span class=\"e\"></span><span>" . $pg->post_title . "</span><span class=\"d\"></span></a>";
 	}
-	$bc .= "<a class=\"last\"><span class=\"e\"></span><span>" . $post->post_title . "</span><span class=\"d\"></span></a>";
+	$bc .= "<a class=\"last\"><span class=\"e\"></span><span class=\"text\">" . $post->post_title . "</span><span class=\"d\"></span></a>";
 	
 	if($container!==false) {
 		$bc = "<{$container} class=\"breadcrumb\">{$bc}</{$container}>";
