c990c182bc5c46ba1b42cc29e2c2b26bc273e214
André Gumieri <andregumieri@gmail.com>
2012-02-09 23:03:44 -0200

    Restaurada a versão original do Kis Minify
    Signed-off-by: André Gumieri <andregumieri@gmail.com>

--- kis/kis_minify.php
+++ kis/kis_minify.php
@@ -5,7 +5,7 @@
  * Functions with prime objective to minify javascript and css files
  *
  * @author André Gumieri
- * @version 1.3
+ * @version 1.2
  *
  * @package KIS
  * @subpackage Minify
@@ -17,41 +17,28 @@
  * Join all files into only one call
  *
  * @author André Gumieri
- * @since 1.1
+ * @since 1.0
  *
  * @param array $items - The array of files to minimize. Each file must have it`s path from theme root. For example: assets/css/common/base.css
  * @param string $type - Type of return (js or css)
  * @param bool $debug - If setted to true, print individual calls for scripts
- * @param bool $echo - Echo the result
- * @param bool $onlyURL - Show only URL instead the complete tag.
  * @return none.
  */
-function kis_minify($items, $type='css', $debug=false, $echo=true, $onlyURL=false) {
+function kis_minify($items, $type='css', $debug=false) {
 	if($type!="js" && $type!="css") {
 		trigger_error("KIS - Minify: {$type} is not a supported minify type. Ignoring this minify output.", E_USER_WARNING);
 		return;
 	}
 	
 	if($debug) {
-		$ret = null;
 		foreach($items as $item) {
-			if( (strpos($item, "http://")!==false || strpos($item, "https://")!==false)) {
-				echo kis_minify_create_tag($type, $item);
-			} else {
-				echo kis_minify_create_tag($type, kis_get_theme_url()."/".$item);
-			}
+			echo kis_minify_create_tag($type, kis_get_theme_url()."/".$item);
 		}
 	} else {
 		$path = kis_get_files_path("minify");
 		$md5all = "";
 		foreach($items as $item) {
-			$itemPath = str_replace(get_bloginfo("url"), "", $item);
-			$itemUrlParsed = parse_url($itemPath);
-			if ( !isset($itemUrlParsed['scheme']) ) {
-				$md5all .= md5_file(ABSPATH.$itemPath);
-			} else {
-				$md5all .= md5(kis_get_file_contents($item));
-			}
+			$md5all.=md5_file(kis_get_theme_path()."/".$item);
 		}
 		
 		$minify_name = md5($md5all).".".$type;
@@ -62,17 +49,7 @@ function kis_minify($items, $type='css', $debug=false, $echo=true, $onlyURL=fals
 				$minify_content .= " * {$item}\n";
 				$minify_content .= " * Created at " . date("Y-m-d H:i:s") . "\n";
 				$minify_content .= " */\n\n";
-				
-				$itemPath = str_replace(get_bloginfo("url"), "", $item);
-				$itemUrlParsed = parse_url($itemPath);
-				$tmp_content = "";
-				if ( !isset($itemUrlParsed['scheme']) ) {
-					$tmp_content = file_get_contents(ABSPATH.$itemPath);
-				} else {
-					$tmp_content = kis_get_file_contents($item);
-				}
-				if(substr($item,0,1)!="/") $itemPath = kis_get_theme_path()."/".$item;
-				
+				$tmp_content = file_get_contents(kis_get_theme_path()."/".$item);
 				if($type=="css") {
 					$url_base = kis_get_theme_url();
 					preg_match_all('/url\([A-z.\/ \'"-_#\?]*\)/', $tmp_content, $out, PREG_PATTERN_ORDER);
@@ -83,7 +60,7 @@ function kis_minify($items, $type='css', $debug=false, $echo=true, $onlyURL=fals
 							$item_path .= "/".$item_path_arr[$x];
 						}
 						$item_path = substr($item_path, 1);
-						$new_o = kis_minify_fix_css_url_path(get_bloginfo("url"), $item_path, $o) . "\n";
+						$new_o = kis_minify_fix_css_url_path(kis_get_theme_url(), $item_path, $o) . "\n";
 						$tmp_content = str_replace($o, $new_o, $tmp_content);
 					}
 					$tmp_content = kis_minify_compress_css($tmp_content);
@@ -93,13 +70,7 @@ function kis_minify($items, $type='css', $debug=false, $echo=true, $onlyURL=fals
 			}
 			file_put_contents($path."/".$minify_name, $minify_content);
 		}
-		
-		$ret = null;
-		if(!$onlyURL) $ret = kis_minify_create_tag($type, kis_get_files_url("minify")."/".$minify_name);
-		else $ret = kis_get_files_url("minify")."/".$minify_name;
-
-		if($echo) echo $ret;
-		else return $ret;
+		echo kis_minify_create_tag($type, kis_get_files_url("minify")."/".$minify_name);
 	}
 }
 
@@ -151,7 +122,7 @@ function kis_minify_compress_css($buffer) {
  * Fix the path for URL tags
  *
  * @author André Gumieri
- * @since 1.3
+ * @since 1.2
  *
  * @param string $prefix - Prefix of the new URL
  * @param string $root - Root of the CSS file
@@ -159,14 +130,6 @@ function kis_minify_compress_css($buffer) {
  * @return string - compressed css content.
  */
 function kis_minify_fix_css_url_path($prefix, $root, $url_line) {
-	$rootPath = str_replace(get_bloginfo("url"), "", $root);
-	$itemUrlParsed = parse_url($rootPath);
-	if( isset($itemUrlParsed['scheme']) ) {
-		return $url_line;
-	}
-	$root = $rootPath;
-	
-	if(substr($root, 0, 1)=="/") $root = substr($root,1);
 	if(substr($prefix, -1)!="/") $prefix .= "/";
 	if(substr($root, -1)=="/") $root = substr($root, 0,-1);
 	$root_arr = explode("/", $root);
@@ -176,43 +139,37 @@ function kis_minify_fix_css_url_path($prefix, $root, $url_line) {
 
 	foreach($arr_url_line as $i_url_line) {
 		$url_line = trim($i_url_line);
-		if( (strpos($url_line, "http://")!==false || strpos($url_line, "https://")!==false) && strpos($url_line, $prefix)===false) {
-			$return .= ",".$url_line;
-		} else {
-			if(strpos($url_line, "url")==0){
-				$abre = strpos($url_line, "(")+1;
-				$fecha = strpos($url_line, ")");
-				$url = substr($url_line, $abre, ($fecha-$abre));
-				$url = str_replace(array("'", "\""), "", $url);
-				
-				
-				if(substr($url, -1)=="/") $url = substr($url, 0,-1);
-				$url_arr = explode("/", $url);
-				
-				$back = 0;
-				$url_new_arr = array();
-				foreach($url_arr as $u) {
-					if($u=="..") $back++;
-					else $url_new_arr[] = $u;
-				}
-				
-				if(isset($url_new_arr[0]) && empty($url_new_arr[0])) {
-					$return .= ",".str_replace($url, implode("/", $url_new_arr), $url_line);
-				} else {
-					
-					$url_new = "";
-					for($x=0; $x<(count($root_arr)-$back); $x++) {
-						$url_new .= "/".$root_arr[$x];
-					}
-					
-					$url_new = $url_new . "/" . implode("/", $url_new_arr);
-					$url_new = $prefix.substr($url_new, 1);
-					
-					$return .= ",".str_replace($url, $url_new, $url_line);
-				}
-			} else { // if(strpos($url_line, "url")==0)
-				$return .= ",".$url_line;
+		if(strpos($url_line, "url")==0){
+			$abre = strpos($url_line, "(")+1;
+			$fecha = strpos($url_line, ")");
+			$url = substr($url_line, $abre, ($fecha-$abre));
+			$url = str_replace(array("'", "\""), "", $url);
+			
+			// Calcula o novo path
+			if(substr($root,0,1)=="/") {
+				// Retorna a string original se a URL começa com barra
+				return $url_line;
 			}
+			
+			if(substr($url, -1)=="/") $url = substr($url, 0,-1);
+			$url_arr = explode("/", $url);
+			
+			$back = 0;
+			$url_new_arr = array();
+			foreach($url_arr as $u) {
+				if($u=="..") $back++;
+				else $url_new_arr[] = $u;
+			}
+			
+			$url_new = "";
+			for($x=0; $x<(count($root_arr)-$back); $x++) {
+				$url_new .= "/".$root_arr[$x];
+			}
+			$url_new = $url_new . "/" . implode("/", $url_new_arr);
+			$url_new = $prefix.substr($url_new, 1);
+			$return .= ",".str_replace($url, $url_new, $url_line);
+		} else {
+			$return .= ",".$url_line;
 		}
 	}
 	
