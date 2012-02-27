1cd46c62aebb7ff06f191d12f7194d53ca1086a2
André Gumieri <andregumieri@elodigital.cc>
2012-02-27 17:45:46 -0300

    Adicionada a library JS Kis.Hash e sua demonstração de uso.
    Signed-off-by: André Gumieri <andregumieri@elodigital.cc>

---
+++ kis/libs/js/_demonstration.js
@@ -0,0 +1,37 @@
+;(function($) {
+	var Demonstration = {
+		init: function() {
+			this.Hash.init();
+		},
+		
+		Hash: {
+			init: function() {
+				var $container = $('<ul id="hashTest" />');
+				$container.append('<li><a href="#/hash1" class="hash1">Hash Teste 1 (/#/hash1)</a></li>');
+				$container.append('<li><a href="#/hash2" class="hash2">Hash Teste 2 (/#/hash2)</a><ul><li><a href="#/hash2/primeiro" class="hash2_primeiro">Hash Teste 2.1 (/#/hash2/primeiro)</a></li><li><a href="#/hash2/segundo" class="hash2_segundo">Hash Teste 2.2 (/#/hash2/segundo)</a><ul><li><a href="#/hash2/segundo/1" class="hash2_segundo_1">Hash Teste 2.2.1 (/#/hash2/segundo/1)</a></li></li></ul></li>');
+				$container.append('<li><a href="#/hash3" class="hash3">Hash Teste 3 (/#/hash3)</a></li>');
+			
+				$("body").append($container);
+				
+				Kis.Hash.adicionaHash("hash1", this.urlChanged);
+				Kis.Hash.adicionaHash("hash2", this.urlChanged);
+				Kis.Hash.adicionaHash("hash3", this.urlChanged);
+				Kis.Hash.init({prefixo: '/'});
+			},
+			
+			urlChanged: function(url, splited) {
+				$("#hashTest").find("a").css("font-weight", "normal");
+				if(splited.length==1) {
+					$("#hashTest").find("."+splited[0]).css("font-weight", "bold");
+				} else if(splited.length==2) {
+					$("#hashTest").find("."+splited[0]+"_"+splited[1]).css("font-weight", "bold");
+				} else if(splited.length==3) {
+					$("#hashTest").find("."+splited[0]+"_"+splited[1]+"_"+splited[2]).css("font-weight", "bold");
+				}
+			}
+		
+		}
+	};
+	
+	$(document).ready(function() { Demonstration.init(); });
+})(jQuery);
---
+++ kis/libs/js/kis.hash.js
@@ -0,0 +1,125 @@
+/**
+ * Funções para controlar hashtags
+ *
+ * @version 1.0
+ * @author André Gumieri
+ */
+ 
+if(!window['Kis']) { var Kis={} }
+(function(namespace, $) {
+	if($==undefined) throw "Sorry, to use Kis.Hash you must use jQuery.";
+	
+	namespace.Hash = {
+		inicial: null,
+		prefixo: null,
+		separador: null,
+		erro404: null,
+		hashs: {},
+		
+		
+		/**
+		 * adicionaHash
+		 * Função que adiciona as hashs para troca pelo Kis.Hash
+		 * e seus callbacks
+		 *
+		 * @param STRING hash: Hash para ser validado. Ex.: blog
+		 * @param FUNCTION callback: O que será executado quando for mudada a hash. Ex.: function(url)
+		 */
+		adicionaHash: function(hash, callback) {
+			this.hashs[hash] = callback;
+		},
+		
+		
+	
+		/**
+		 * init
+		 * Função que inicia o controle de hashtags
+		 *
+		 * @param	OBJ		opcoes
+		 *					inicial		STRING		Página inicial para quando a HASH for vazia. Default: ''
+		 *					prefixo		STRING		Prefixo da URL, ou seja, string que vai depois do #.
+		 *											Ex.: '/' fica #/url
+		 *					separador	STRING		Separador de sub urls. Default: /. Ex.: #blog/post-do-blog
+		 *					erro404		FUNCTION	Função de callback para quando der erro 404
+		 */
+		init: function(opcoes) {
+			var self = this;
+			var settings = {
+				'inicial': '',
+				'prefixo': '',
+				'separador': '/',
+				'erro404': this.erro404
+			}
+			$.extend(settings, opcoes);
+			
+			this.inicial = settings.inicial;
+			this.prefixo = settings.prefixo;
+			this.separador = settings.separador;
+			this.erro404 = settings.erro404;
+			
+			$(window).bind("hashchange", function() { self.executaCallback() } );
+			if( location.hash=="" || location.hash=="#" || location.hash=="#"+self.hashPrefix ) {
+				if(self.inicial) {
+					self.mudaHash(self.inicial);
+				}
+			} else {
+				self.executaCallback();
+			}
+		},
+		
+		
+		
+		/**
+		 * executaCallback
+		 * Executa a função de callback para URL
+		 */
+		executaCallback: function() {
+			var self = this;
+			var ignore = "#"+this.prefixo;
+			var hash = location.hash.substr(ignore.length);
+			
+			// Se o hash estiver terminando com o separador, tira.
+			if(hash.substr(-1)==this.separador) {
+				hash = hash.substr(0,hash.length-1);
+			}
+			
+			// Pega apenas a primeira parte do hash
+			var hashSplited = hash.split(self.separador);
+			var hashPrimeiraParte = hashSplited[0];
+			
+			// Pega a função de ação da URL
+			var action = self.hashs[hashPrimeiraParte];
+			
+			
+			// Valida se a URL existe
+			if(typeof(action)=="undefined") {
+				alert("Erro 404: Página não encontrada");
+			} else {
+				action.call(self, hash, hashSplited);
+			}
+		},
+		
+		
+		/**
+		 * mudarHash
+		 * Muda o HASH no browser nos padrões necessários para
+		 * funcionamento desta classe
+		 *
+		 * @param STRING pagina: Mandar como parametro apenas a página, sem #
+		 */
+		mudarHash: function(pagina) {
+			location.hash = this.prefixo + pagina;
+		},
+		
+		
+		
+		/**
+		 * erro404
+		 * Mensagem para quando a página não existe
+		 */
+		erro404: function() {
+			alert("Erro 404: Página não encontrada");
+		}
+
+	}
+})(Kis, jQuery);
